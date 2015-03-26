706e670e1af8bda4f41bd92ffbc25c214d32b396
Kenshin <duongnv.hust.92@gmail.com>
2015-03-26 14:41:59 +0700

    feature git add

--- inventory/index.php
+++ inventory/index.php
@@ -1,3 +1,23 @@
 <?php 
 
-echo 111111;
+echo 111111."qadas";
+
+sssssssssssADAS 
+ASFASFAS
+echo "Duongnvafafs";
+
+
+babababfsjfb 
+duognnvsdv
+
+
+
+echo 111111."34567890987654";
+
+sssssssssssADAS 
+ASFASFAS
+echo "Duongnvafafs";
+
+
+babababfsjfb 
+duognnvsdv
